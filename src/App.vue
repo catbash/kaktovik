413b50c2be3c4ef06219a7833a698dbf0f3f3ef5
asterein <88514774+asterein@users.noreply.github.com>
2022-09-06 03:26:48 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,126 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+  import { ref } from 'vue';
+  import KaktovikImg from "./components/Kaktovik.vue";
+  
+  const base10 = ref('');
+  const base20 = ref('');
+  const alert = ref('');
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  function ten2twenty () {
+    base20.value = '';
+    if (base10.value === '') return;
+    const intRegex = /^\d+$/;
+    if (intRegex.test(base10.value)) {
+      base20.value = Number(base10.value).toString(20);
+    } else {
+      base10.value = '';
+      alert.value = 'Please enter a whole number!';
+    }
   }
-}
+
+  function reset () {
+    alert.value = '';
+  }
+
 </script>
 
+<template>
+  <div id="app">
+    <div id="app-contents">
+      <form @submit.prevent="ten2twenty">
+        <input type="text" @click="reset" v-model="base10" placeholder="Enter a number..." />
+        <button>Kaktovik Iñupiaq</button>
+      </form>
+      <div class="alert">{{ alert }}</div>
+      <div class="kaktovik">
+        <div class="kaktovik-num" v-for="char in base20.split('')" :key="char.index">
+          <KaktovikImg :number="char" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  :root, * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    color: #fff;
+  }
+
+  html, body {
+    background: #111;
+  }
+
+  button {
+    cursor: pointer;
+    margin-top: 0.5rem;
+    background: transparent;
+    outline: 1px solid #fff;
+    border: none;
+    border-radius: 3px;
+    padding: 0.15rem 0.5rem;
+    opacity: 0.5;
+  }
+
+  button:hover {
+    opacity: 1;
+    background: #fff;
+    color: #111;
+  }
+
+  h1 {
+    margin-bottom: 1rem;
+  }
+
+  input, input:focus {
+    display: block;
+    padding: 0.5rem;
+    border-radius: 3px;
+    margin: 0 auto;
+    background: #111;
+    border: 1px solid #fff;
+    outline: none;
+    opacity: 0.5;
+    text-align: center;
+  }
+
+  input:hover {
+    opacity: 1;
+  }
+
+  input:focus {
+    background: #222;
+  }
+
+  .alert {
+    color: red;
+    font-size: 0.8rem;
+    margin-top: 0.5rem;
+  }
+
+  #app {
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    align-items: center;
+  }
+
+  #app-contents {
+    text-align: center;
+    margin: 0 auto;
+    min-height: 235px;
+    padding: 1rem;
+  }
+
+  .kaktovik {
+    text-align: center;
+  }
+
+  .kaktovik-num {
+    display: inline-block;
+    filter: invert();
+  }
 </style>
---
+++ src/components/Kaktovik.vue
@@ -0,0 +1,64 @@
+<script>
+  import zero from "../assets/kaktovik/0.svg";
+  import one from "../assets/kaktovik/1.svg";
+  import two from "../assets/kaktovik/2.svg";
+  import three from "../assets/kaktovik/3.svg";
+  import four from "../assets/kaktovik/4.svg";
+  import five from "../assets/kaktovik/5.svg";
+  import six from "../assets/kaktovik/6.svg";
+  import seven from "../assets/kaktovik/7.svg";
+  import eight from "../assets/kaktovik/8.svg";
+  import nine from "../assets/kaktovik/9.svg";
+  import ten from "../assets/kaktovik/10.svg";
+  import eleven from "../assets/kaktovik/11.svg";
+  import twelve from "../assets/kaktovik/12.svg";
+  import thirteen from "../assets/kaktovik/13.svg";
+  import fourteen from "../assets/kaktovik/14.svg";
+  import fifteen from "../assets/kaktovik/15.svg";
+  import sixteen from "../assets/kaktovik/16.svg";
+  import seventeen from "../assets/kaktovik/17.svg";
+  import eighteen from "../assets/kaktovik/18.svg";
+  import nineteen from "../assets/kaktovik/19.svg";
+
+  const kaktovik = {
+    0: zero,
+    1: one,
+    2: two,
+    3: three,
+    4: four,
+    5: five,
+    6: six,
+    7: seven,
+    8: eight,
+    9: nine,
+    a: ten,
+    b: eleven,
+    c: twelve,
+    d: thirteen,
+    e: fourteen,
+    f: fifteen,
+    g: sixteen,
+    h: seventeen,
+    i: eighteen,
+    j: nineteen
+  }
+
+  export default {
+    name: 'kaktovikImg',
+    props: {
+      number: String
+    },
+    methods: {
+      getKaktovik (num) {
+        return kaktovik[num];
+      }
+    }
+  }
+</script>
+
+<template>
+  <img :src="getKaktovik(number)" />
+</template>
+
+<style scoped>
+</style>
